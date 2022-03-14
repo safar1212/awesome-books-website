f34bebf024fafc29ea9923cb6590bb07bdddd589
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-03-14 17:10:04 +0300

    Create the adding books part

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 const books = [
   {
-    title: 'Grow and think rich',
-    author: 'John',
+    title: "Grow and think rich",
+    author: "John",
   },
   {
-    title: 'The persue to happiness',
-    author: 'Chris Gardener',
+    title: "The persue to happiness",
+    author: "Chris Gardener",
   },
 ];
 
-let booksContainer = document.getElementById('books-container');
+let booksContainer = document.getElementById("books-container");
+const addBookForm = document.getElementById("add-book");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
 
+const reload = () => {
+  booksContainer.innerHTML = books
+    .map((bookItem) => {
+      return `<p>${bookItem.title}</p>
+      <p>${bookItem.author}.</p>
+      <button>Remove</button>
+      <hr>`;
+    })
+    .join("");
+};
 
-booksContainer.innerHTML = books.map(bookItem => {
-    return `<p>${bookItem.title}</p>
-    <p>${bookItem.author}.</p>
-    <button>Remove</button>
-    <hr>`
-}). join('');
+reload();
+
+addBookForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let newBook = {
+    title: title.value,
+    author: author.value,
+  };
+  books.push(newBook);
+
+  reload();
+
+});
