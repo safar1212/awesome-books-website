75b3f853259bcb53a404a7f7f78ec4b030b87d27
safar1212 <mm9240584@gmail.com>
2022-03-14 19:50:35 +0500

    Fix the linters errors

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-let books = localStorage.getItem("books")
-  ? JSON.parse(localStorage.getItem("books"))
+let books = localStorage.getItem('books')
+  ? JSON.parse(localStorage.getItem('books'))
   : [];
 
-let booksContainer = document.getElementById("books-container");
-const addBookForm = document.getElementById("add-book");
-const title = document.getElementById("title");
-const author = document.getElementById("author");
+const booksContainer = document.getElementById('books-container');
+const addBookForm = document.getElementById('add-book');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
 
 const reload = () => {
   booksContainer.innerHTML = books
-    .map((bookItem, index) => {
-      return `<p>${bookItem.title}</p>
+    .map((bookItem, index) => `<p>${bookItem.title}</p>
       <p>${bookItem.author}.</p>
       <button onclick="removeBook(${index})">Remove</button>
-      <hr>`;
-    })
-    .join("");
+      <hr>`)
+    .join('');
 };
 
 reload();
 
-addBookForm.addEventListener("submit", (event) => {
+addBookForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  let newBook = {
+  const newBook = {
     title: title.value,
     author: author.value,
   };
   books.push(newBook);
 
-  localStorage.setItem("books", JSON.stringify(books));
+  localStorage.setItem('books', JSON.stringify(books));
 
   reload();
 });
-
+/* eslint-disable no-unused-vars */
 const removeBook = (bookIndex) => {
   books = books.filter((item, index) => {
     if (index !== bookIndex) {
       return item;
     }
+    return undefined;
   });
-  localStorage.setItem("books", JSON.stringify(books));
+  localStorage.setItem('books', JSON.stringify(books));
   reload();
 };
+/* eslint-disable no-unused-vars */
