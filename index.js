b327ee279fcd30381f79b1485ddac9a8d2d7f4d9
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-03-15 15:09:06 +0300

    Refactor code to use javascript class

--- index.js
+++ index.js
@@ -1,44 +1,62 @@
-let books = localStorage.getItem('books')
-  ? JSON.parse(localStorage.getItem('books'))
-  : [];
+class Books {
+  constructor() {
+    this.books = localStorage.getItem("books")
+    ? JSON.parse(localStorage.getItem("books"))
+    : [];
+  }
 
-const booksContainer = document.getElementById('books-container');
-const addBookForm = document.getElementById('add-book');
-const title = document.getElementById('title');
-const author = document.getElementById('author');
+  addBook(book) {
+    this.books.push(book);
+
+    localStorage.setItem("books", JSON.stringify(this.books));
+  }
+
+  removeBook(bookIndex) {
+    this.books = this.books.filter((item, index) => {
+      if (index !== bookIndex) {
+        return item;
+      }
+      return undefined;
+    });
+    localStorage.setItem("books", JSON.stringify(this.books));
+  }
+}
+
+let allBooks = new Books();
+
+const booksContainer = document.getElementById("books-container");
+const addBookForm = document.getElementById("add-book");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
 
 const reload = () => {
-  booksContainer.innerHTML = books
-    .map((bookItem, index) => `<p>${bookItem.title}</p>
+  booksContainer.innerHTML = allBooks.books
+    .map(
+      (bookItem, index) => `<p>${bookItem.title}</p>
       <p>${bookItem.author}.</p>
       <button onclick="removeBook(${index})">Remove</button>
-      <hr>`)
-    .join('');
+      <hr>`
+    )
+    .join("");
 };
 
 reload();
 
-addBookForm.addEventListener('submit', (event) => {
+console.log(allBooks.books);
+
+addBookForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const newBook = {
     title: title.value,
     author: author.value,
   };
-  books.push(newBook);
-
-  localStorage.setItem('books', JSON.stringify(books));
+  allBooks.addBook(newBook);
 
   reload();
 });
 /* eslint-disable no-unused-vars */
 const removeBook = (bookIndex) => {
-  books = books.filter((item, index) => {
-    if (index !== bookIndex) {
-      return item;
-    }
-    return undefined;
-  });
-  localStorage.setItem('books', JSON.stringify(books));
+  allBooks.removeBook(bookIndex);
   reload();
 };
 /* eslint-disable no-unused-vars */
