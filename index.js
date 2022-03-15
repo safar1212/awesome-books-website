cb048b5d091276df3c50febefab5663465e8465c
safar1212 <mm9240584@gmail.com>
2022-03-15 18:34:12 +0500

    Styled the webpage and resolved the linters errors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 class Books {
   constructor() {
-    if(localStorage.getItem('books') === null) {
+    if (localStorage.getItem('books') === null) {
       this.books = [];
     } else {
       this.books = JSON.parse(localStorage.getItem('books'));
@@ -10,7 +10,7 @@ class Books {
   addBook(book) {
     this.books.push(book);
 
-    localStorage.setItem("books", JSON.stringify(this.books));
+    localStorage.setItem('books', JSON.stringify(this.books));
   }
 
   removeBook(bookIndex) {
@@ -21,31 +21,30 @@ class Books {
       return undefined;
     });
 
-    localStorage.setItem("books", JSON.stringify(this.books));
+    localStorage.setItem('books', JSON.stringify(this.books));
   }
 }
 
-let allBooks = new Books();
+const allBooks = new Books();
 
-const booksContainer = document.getElementById("books-container");
-const addBookForm = document.getElementById("add-book");
-const title = document.getElementById("title");
-const author = document.getElementById("author");
+const booksContainer = document.getElementById('books-container');
+const addBookForm = document.getElementById('add-book');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
 
 function reload() {
   booksContainer.innerHTML = allBooks.books
     .map(
-      (bookItem, index) => `<p>${bookItem.title}</p>
-      <p>${bookItem.author}.</p>
+      (bookItem, index) => `<div class="book-item"><p><strong>"${bookItem.title}" by ${bookItem.author}.</strong></p>
       <button onclick="removeBook(${index})">Remove</button>
-      <hr>`
+      </div>`,
     )
-    .join("");
-};
+    .join('');
+}
 
 reload();
 
-addBookForm.addEventListener("submit", (event) => {
+addBookForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const newBook = {
     title: title.value,
