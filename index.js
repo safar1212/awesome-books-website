f69cc511ee89eab52dbf0009c1099cd01bc5ba94
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-03-16 11:59:16 +0300

    Fix border problem

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 class Books {
   constructor() {
-    if (localStorage.getItem('books') === null) {
+    if (localStorage.getItem("books") === null) {
       this.books = [];
     } else {
-      this.books = JSON.parse(localStorage.getItem('books'));
+      this.books = JSON.parse(localStorage.getItem("books"));
     }
   }
 
   addBook(book) {
     this.books.push(book);
 
-    localStorage.setItem('books', JSON.stringify(this.books));
+    localStorage.setItem("books", JSON.stringify(this.books));
   }
 
   removeBook(bookIndex) {
@@ -21,38 +21,47 @@ class Books {
       return undefined;
     });
 
-    localStorage.setItem('books', JSON.stringify(this.books));
+    localStorage.setItem("books", JSON.stringify(this.books));
+
   }
 }
 
-const allBooks = new Books();
+const booksContainer = document.getElementById("books-container");
+const addBookForm = document.getElementById("add-book");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
 
-const booksContainer = document.getElementById('books-container');
-const addBookForm = document.getElementById('add-book');
-const title = document.getElementById('title');
-const author = document.getElementById('author');
+const allBooks = new Books();
 
 function reload() {
   booksContainer.innerHTML = allBooks.books
     .map(
-      (bookItem, index) => `<div class="book-item"><p><strong>"${bookItem.title}" by ${bookItem.author}.</strong></p>
+      (
+        bookItem,
+        index
+      ) => `<div class="book-item"><p><strong>"${bookItem.title}" by ${bookItem.author}.</strong></p>
       <button onclick="removeBook(${index})">Remove</button>
-      </div>`,
+      </div>`
     )
-    .join('');
+    .join("");
+  if(allBooks.books.length === 0) {
+    booksContainer.style.cssText = "border: none;";
+  } else {
+    booksContainer.style.cssText = "border: 3px black solid;";
+  }
 }
 
 reload();
 
-addBookForm.addEventListener('submit', (event) => {
+addBookForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const newBook = {
     title: title.value,
     author: author.value,
   };
   allBooks.addBook(newBook);
-  title.value = '';
-  author.value = '';
+  title.value = "";
+  author.value = "";
   reload();
 });
 /* eslint-disable no-unused-vars */
